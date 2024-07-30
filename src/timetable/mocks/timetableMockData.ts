24d4bbd223fb049752a9e12e4e95111edef02e69
dbshaejin <dbshaejin@gmail.com>
2024-07-30 17:01:34 +0900

    feat: timetable 기초 구조 세팅

---
+++ src/timetable/mocks/timetableMockData.ts
@@ -0,0 +1,26 @@
+export const taskList = [
+  {
+    id: 1,
+    title: 'title1',
+    subTitle: 'subTitle1',
+    slotColor: 'red',
+    startTime: new Date('Tue Jul 30 2024 15:28:01 GMT+0900'),
+    endTime: new Date('Tue Jul 30 2024 15:28:01 GMT+0900'),
+  },
+  {
+    id: 2,
+    title: 'title2',
+    subTitle: 'subTitle2',
+    slotColor: 'blue',
+    startTime: new Date('Tue Jul 30 2024 15:28:01 GMT+0900'),
+    endTime: new Date('Tue Jul 30 2024 15:28:01 GMT+0900'),
+  },
+  {
+    id: 3,
+    title: 'title3',
+    subTitle: 'subTitle3',
+    slotColor: 'green',
+    startTime: new Date('Tue Jul 30 2024 15:28:01 GMT+0900'),
+    endTime: new Date('Tue Jul 30 2024 15:28:01 GMT+0900'),
+  },
+];
