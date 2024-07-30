5aa5eb4fa9be2fddd55404f259dfb6ede8e5d71b
dbshaejin <dbshaejin@gmail.com>
2024-07-30 23:51:33 +0900

    feat: slot 컴포넌트 인자로 넘길 요소 로직 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,21 @@
-import Image from 'next/image';
-import styles from './page.module.css';
+// import Image from 'next/image';
+// import styles from './page.module.css';
+import { taskList } from '@/timetable/mocks/timetableMockData';
+import Timetable from '@/timetable/components/Timetable';
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const data = new Date();
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-    </main>
+  console.log('page  sample', data.toLocaleString('ko-KR', { timeZone: 'Asia/Seoul' }));
+  return (
+    <Timetable
+      startTime={data}
+      endTime={data}
+      slotTime={30}
+      height="800px"
+      timetableType="COLUMN"
+      displayCurrentTime
+      taskList={taskList}
+    />
   );
 }
--- src/timetable/components/Timetable.tsx
+++ src/timetable/components/Timetable.tsx
@@ -1,3 +1,4 @@
+import { eachMinuteOfInterval, endOfDay, startOfDay } from 'date-fns';
 import styled from './Timetable.module.scss';
 
 interface TimetableProps {
@@ -10,6 +11,7 @@ interface TimetableProps {
   taskList: Task[];
 }
 
+// 타입 관련해서 nullable한 값 고려하기
 interface Task {
   id: number;
   title: string;
@@ -28,13 +30,54 @@ function Timetable({
   displayCurrentTime,
   taskList,
 }: TimetableProps) {
-  console.log('taskList', taskList);
-  console.log('timetableType', timetableType);
-  console.log('displayCurrentTime', displayCurrentTime);
-  console.log('height', height);
-  console.log('slotTime', slotTime);
-  console.log('endTime', endTime);
-  console.log('startTime', startTime);
+  const now = new Date();
+  console.log(startTime, endTime, slotTime, height, timetableType, displayCurrentTime, taskList);
+  // lint에러 막기 위한 console
+
+  const startTimeDateObject = startOfDay(now); // 오늘의 시작 시간 (00:00)
+
+  const endTimeDateObject = endOfDay(now); // 오늘의 끝 시간 (23:59)
+
+  // const START_TIME = set(now, {
+  //   // 시작 시작 시간을 찾는 것
+  //   hours: 9,
+  //   minutes: 0,
+  //   seconds: 0,
+  //   milliseconds: 0,
+  // }); // 09:00
+  // const END_TIME = set(now, {
+  //   hours: 17,
+  //   minutes: 0,
+  //   seconds: 0,
+  //   milliseconds: 0,
+  // }); // 17:00
+
+  const timeSlots = eachMinuteOfInterval(
+    {
+      start: startTimeDateObject,
+      end: endTimeDateObject,
+    },
+    { step: slotTime },
+  );
+
+  const taskListFilter = (taskListInput: Task[], checkHour: number, slotTimeInput: number) =>
+    taskListInput.filter(task => {
+      // 작업의 시작 시간과 종료 시간을 24시간 형식으로 가져오기
+      const taskStartHour = task.startTime.getHours();
+      const taskEndHour = task.endTime.getHours();
+      const taskEndMinute = task.endTime.getMinutes();
+
+      // 필터링 조건
+      // 시작 시간이 checkHour보다 작고 종료 시간이 checkHour보다 큰 작업
+      // 종료 시간의 분(taskEndMinute)이 slotTimeInput과 같지 않은 경우를 제외
+      return (
+        taskStartHour <= checkHour &&
+        taskEndHour >= checkHour &&
+        !(taskEndHour === checkHour && taskEndMinute === slotTimeInput)
+      );
+    });
+
+  console.log('taskListFiltertaskListFilter', taskListFilter(taskList, 17, slotTime)); // 이게 slot으로 넘길 녀석
 
   return (
     <div>
@@ -42,10 +85,11 @@ function Timetable({
         <h1>Timetable</h1>
       </div>
       <div>
-        <div>slot이 될 녀석</div>
+        {timeSlots.map(time => (
+          // key 고려필요
+          <div key={time}>{time.getHours()}</div>
+        ))}
         {/* props => headerDate, soltTime, 필더링 된 taskitem?가 넘어올 예정. */}
-        <div>slot이 될 녀석</div>
-        <div>slot이 될 녀석</div>
       </div>
       {/* <div>
         {taskList.map(task => (
