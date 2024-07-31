0e81f3584d234a26b43ee736e7e4392c57d5e90c
godhyzzang <109357302+godhyzzang@users.noreply.github.com>
2024-07-31 23:23:26 +0900

    refactor : 수정

--- src/components/common/Text.styled.ts
+++ src/components/common/Text.styled.ts
@@ -16,11 +16,11 @@ interface TextProps {
   $decorationLine?: string;
   theme?: {
     fontSize: {
-      s: string;
-      m: string;
-      l: string;
+      sm: string;
+      md: string;
+      lg: string;
       xl: string;
-      '2xl': string;
+      xxl: string;
       xxxl: string;
     };
     fontWeight: {
@@ -42,7 +42,7 @@ const Text = styled.span<TextProps>`
   width: ${(props) => (props?.$width !== undefined ? props.$width : 'fit-content')};
   height: ${(props) => (props?.$height !== undefined ? props.$height : 'auto')};
   line-height: ${(props) => (props?.$lineHeight ? props.$lineHeight : '100%')};
-  font-size: ${(props) => (props?.$fontSize ? props.$fontSize : props.theme?.fontSize['2xl'])};
+  font-size: ${(props) => (props?.$fontSize ? props.$fontSize : props.theme?.fontSize.md)};
   font-weight: ${(props) => (props?.$fontWeight ? props.$fontWeight : props.theme?.fontWeight.bold)};
   color: ${(props) => (props?.$color ? props.$color : props.theme?.color.fontColor)};
   text-decoration-line: ${(props) => (props?.$decorationLine ? props.$decorationLine : 'none')};
