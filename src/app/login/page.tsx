73aa7c123b313d5b60442a6b5d4836d058b5e797
Yejin0O0 <yyj11kr@naver.com>
2024-07-31 16:13:38 +0900

    feat: 로그인 페이지 ui 완성

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import LoginSection from '@/components/login/index';
+import * as S from './loginPage.styled';
+
+export default function Login() {
+  return (
+    <S.LoginPage>
+      <LoginSection />
+    </S.LoginPage>
+  );
+}
