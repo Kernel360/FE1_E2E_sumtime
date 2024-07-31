c151e5cb1e0e8af914c3214bb27f3e3969ba6b59
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-07-31 13:04:56 +0900

    feat: TimeSlot 컴포넌트 구현

---
+++ src/timetable/components/TaskSlot.tsx
@@ -0,0 +1,49 @@
+import { add } from 'date-fns';
+import styled from './Slot.module.scss';
+import { calculateTaskOffsetAndHeightPercent } from '@/timetable/utils';
+import type { Task } from './Timetable.type';
+
+interface TaskSlotProps {
+  headerDate: Date;
+  slotTime: number;
+  taskItem: Task | undefined;
+}
+
+function TaskSlot({ headerDate, slotTime, taskItem }: TaskSlotProps) {
+  if (!taskItem) {
+    return <div className={styled.taskSlotLayout} />;
+  }
+
+  const { startTime, endTime, slotColor, title, subTitle } = taskItem;
+
+  const slotStartTime = headerDate;
+  const slotEndTime = add(headerDate, { minutes: slotTime });
+
+  const { offset, heightPercent } = calculateTaskOffsetAndHeightPercent(
+    slotStartTime,
+    slotEndTime,
+    startTime,
+    endTime,
+    slotTime,
+  );
+
+  return (
+    <div className={styled.taskSlotLayout}>
+      <div
+        className={styled.taskSlotBackground}
+        style={{
+          top: `${offset}%`,
+          left: '0',
+          height: `${heightPercent}%`,
+          backgroundColor: `${slotColor}`,
+        }}
+      />
+      <div className={styled.taskSlotContent}>
+        <p className={styled.title}>{title}</p>
+        <p className={styled.description}>{subTitle}</p>
+      </div>
+    </div>
+  );
+}
+
+export default TaskSlot;
--- src/timetable/utils/index.ts
+++ src/timetable/utils/index.ts
@@ -10,4 +10,34 @@ const getHourAndMinutesFormat = (data: Date) => {
 // 시간을 분단위로 바꿔버리고 더해주는 함수
 const sumHoursAndMinutes = (date: Date) => date.getHours() * 60 + date.getMinutes();
 
-export { getHourAndMinutesFormat, sumHoursAndMinutes };
+const calculateTaskOffsetAndHeightPercent = (
+  slotStartTime: Date,
+  slotEndTime: Date,
+  taskStartTime: Date,
+  taskEndTime: Date,
+  slotTime: number,
+) => {
+  const slotStartMinutes = sumHoursAndMinutes(slotStartTime);
+  const slotEndMinutes = sumHoursAndMinutes(slotEndTime);
+  const taskStartMinutes = sumHoursAndMinutes(taskStartTime);
+  const taskEndMinutes = sumHoursAndMinutes(taskEndTime);
+
+  let offset = 0;
+  let endPercent = 100;
+
+  if (slotStartMinutes < taskStartMinutes) {
+    // 슬롯의 시작시간보다 task의 시작 시간이 늦었다면(즉 slot 도중에 시작했다면)
+    offset = ((taskStartMinutes - slotStartMinutes) / slotTime) * 100;
+  }
+
+  if (taskEndMinutes < slotEndMinutes) {
+    // task의 끝나는 시간이 slot의 종료 시간보다 늦다면(즉 slot 도중에 끝난다면)
+    endPercent = ((taskEndMinutes - slotStartMinutes) / slotTime) * 100;
+  }
+
+  const heightPercent = endPercent - offset;
+
+  return { offset, heightPercent };
+};
+
+export { getHourAndMinutesFormat, sumHoursAndMinutes, calculateTaskOffsetAndHeightPercent };
