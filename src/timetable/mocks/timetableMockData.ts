365e265521355c4a8bbf1a895cb8e4ccea494974
해진 <86779590+haejinyun@users.noreply.github.com>
2024-07-31 17:41:40 +0900

    feat: timetable, slot 연동 (#29)
    * feat: slot 컴포넌트 연결
    
    * chore: 주석 제거

--- src/timetable/mocks/timetableMockData.ts
+++ src/timetable/mocks/timetableMockData.ts
@@ -1,26 +1,41 @@
-export const taskList = [
+const getDateFromTime = (hours: number, minutes: number, second: number) => {
+  const yearMonthDay = '2024-07-30';
+
+  const hourFormat = hours < 10 ? `0${hours}` : hours;
+  const minutesFormat = minutes < 10 ? `0${minutes}` : minutes;
+  const secondeFormat = second < 10 ? `0${second}` : second;
+
+  return new Date(`${yearMonthDay}T${hourFormat}:${minutesFormat}:${secondeFormat}`);
+};
+
+const startTime = getDateFromTime(0, 0, 1);
+const endTime = getDateFromTime(23, 59, 59);
+const slotTime = 60;
+
+const taskList = [
   {
     id: 1,
     title: 'title1',
     subTitle: 'subTitle1',
     slotColor: 'red',
-    startTime: new Date('2024-07-30T06:30:00.556Z'),
-    endTime: new Date('2024-07-30T06:50:00.556Z'),
+    startTime: getDateFromTime(12, 0, 0),
+    endTime: getDateFromTime(13, 0, 0),
   },
   {
     id: 2,
     title: 'title2',
     subTitle: 'subTitle2',
     slotColor: 'blue',
-    startTime: new Date('2024-07-30T07:00:00.556Z'),
-    endTime: new Date('2024-07-30T07:30:00.556Z'),
+    startTime: getDateFromTime(15, 0, 0),
+    endTime: getDateFromTime(16, 0, 0),
   },
   {
     id: 3,
     title: 'title3',
     subTitle: 'subTitle3',
-    slotColor: 'perple',
-    startTime: new Date('2024-07-30T07:40:00.556Z'),
-    endTime: new Date('2024-07-30T09:30:00.556Z'),
+    slotColor: 'purple',
+    startTime: getDateFromTime(18, 0, 0),
+    endTime: getDateFromTime(20, 0, 0),
   },
 ];
+export { startTime, endTime, taskList, slotTime };
