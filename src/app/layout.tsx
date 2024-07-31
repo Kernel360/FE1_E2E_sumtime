9bb1ce09c14fb5e567c29692993afe31649dfae8
Yejin0O0 <yyj11kr@naver.com>
2024-07-31 16:32:20 +0900

    fix: global styles 제거 및 globals.css 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
+import './globals.css';
 
 export const metadata: Metadata = {
   title: 'sumday',
@@ -15,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="kr">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
