ec5abc8a86e1b2921c133978254b11be730bf4e0
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-08-01 14:56:05 +0900

    fix: lint 에러 수정

--- src/timetable/components/Timetable.tsx
+++ src/timetable/components/Timetable.tsx
@@ -25,23 +25,6 @@ interface Task {
   endTime: Date;
 }
 
-const calculateSlotHeight = (height: string, totalSlots: number) => {
-  // 각 slot의 높이를 계산하는 함수 (각 slot이 동일한 height를 가지고 있어야하기에)
-  if (height.endsWith('px')) {
-    // px 단위인 경우
-    const totalHeightPx = parseInt(height, 10);
-    return `${totalHeightPx / totalSlots}px`;
-  }
-  if (height.endsWith('%')) {
-    // % 단위인 경우
-    const totalHeightPercent = parseInt(height, 10);
-    return `${totalHeightPercent / totalSlots}%`;
-  }
-  // 기본 높이 설정 (예: px)
-  const defaultHeight = 1000; // px
-  return `${defaultHeight / totalSlots}px`;
-};
-
 const taskListFilter = (taskListInput: Task[], checkHour: number, slotTimeInput: number) =>
   taskListInput.filter((task: Task) => {
     const taskStartHour = task.startTime.getHours();
@@ -56,6 +39,8 @@ const taskListFilter = (taskListInput: Task[], checkHour: number, slotTimeInput:
   });
 
 function Timetable({ startTime, endTime, slotTime, height, timetableType, displayCurrentTime, taskList }: TimetableProps) {
+  console.log(timetableType);
+
   const timeSlots = eachMinuteOfInterval(
     {
       start: startTime,
