cf1b164b1bb49c1c15b3de9da0697710d7b7c358
Jason <enigsuss@gmail.com>
2024-08-01 14:24:15 +0900

    feat: read todo api 및 테스트 코드 작성

---
+++ src/app/api/todo/read/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { eq } from 'drizzle-orm';
+import { db, schema } from '../../../../db';
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const userId = searchParams.get('userId');
+
+  if (!userId) {
+    return NextResponse.json({ error: 'UserID query parameter is required' }, { status: 400 });
+  }
+
+  console.log('Received id:', userId);
+
+  try {
+    const todos = await db
+      .select()
+      .from(schema.todosTable)
+      .where(eq(schema.todosTable.userId, parseInt(userId, 10)))
+      .all();
+
+    console.log('Query result:', todos);
+
+    if (todos.length > 0) {
+      return NextResponse.json({ todos });
+    }
+    return NextResponse.json({ error: 'Todos not found' }, { status: 404 });
+  } catch (error) {
+    console.error('Error fetching Todo:', error);
+    return NextResponse.json({ error: 'Failed to fetch Todos', details: (error as Error).message }, { status: 500 });
+  }
+}
--- src/app/apiTest/page.tsx
+++ src/app/apiTest/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, FormEvent } from 'react';
 
 export default function Login() {
   const [email, setEmail] = useState('');
@@ -12,11 +12,12 @@ export default function Login() {
   const [startTime, setStartTime] = useState('');
   const [endTime, setEndTime] = useState('');
   const [color, setColor] = useState('');
+  const [todoId, setTodoId] = useState('');
 
   const createUser = async (event: React.FormEvent) => {
     event.preventDefault();
 
-    const response = await fetch('/api/user/add', {
+    const response = await fetch('/api/user/create', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -80,24 +81,66 @@ export default function Login() {
       alert(`Error: ${errorData.error}`);
     }
   };
-  const createTodo = async (event: React.FormEvent) => {
+  const todoSubmitHandler = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    const submitter = (event.nativeEvent as SubmitEvent).submitter as HTMLButtonElement;
+    const { name } = submitter;
 
-    const response = await fetch('/api/todo/add', {
-      method: 'POST',
+    let endpoint = '';
+    let method = '';
+    let body: string | undefined;
+
+    switch (name) {
+      case 'create':
+        endpoint = '/api/todo/create';
+        method = 'POST';
+        body = JSON.stringify({ userId, title, content, startTime, endTime, color });
+        break;
+      case 'read':
+        endpoint = `/api/todo/read?userId=${userId}`;
+        method = 'GET';
+        break;
+      case 'update':
+        endpoint = '/api/todo/update';
+        method = 'PUT';
+        body = JSON.stringify({ userId, title, content, startTime, endTime, color });
+        break;
+      case 'delete':
+        endpoint = '/api/todo/delete';
+        method = 'DELETE';
+        body = JSON.stringify({ userId, title, content, startTime, endTime, color });
+        break;
+      default:
+        alert('Unknown action');
+        return;
+    }
+    const response = await fetch(endpoint, {
+      method,
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ userId, title, content, startTime, endTime, color }),
+      body,
     });
 
     if (response.ok) {
-      alert('Todo added successfully');
+      const data = await response.json();
+      alert(`${name.charAt(0).toUpperCase() + name.slice(1)} action was successful`);
+      alert(JSON.stringify(data, null, 2));
+      if (name === 'read' || name === 'update') {
+        setUserId(data.todos[0].userId);
+        setTodoId(data.todos[0].todoId);
+        setTitle(data.todos[0].title);
+        setContent(data.todos[0].content);
+        setStartTime(data.todos[0].startTime);
+        setEndTime(data.todos[0].endTime);
+        setColor(data.todos[0].color);
+      }
     } else {
       const errorData = await response.json();
-      alert(`Failed to add Todo: ${errorData.error}`);
+      alert(`Failed to ${name} Todo: ${errorData.error}`);
     }
   };
+
   return (
     <div>
       <h1 style={{ color: 'orange' }}>Create User</h1>
@@ -124,15 +167,31 @@ export default function Login() {
         <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
         <button type="submit">Can I Login?</button>
       </form>
-      <h1 style={{ color: 'orange' }}>Create Todo</h1>
-      <form onSubmit={createTodo} style={{ display: 'flex', flexDirection: 'column', width: '300px' }}>
-        <input type="text" onChange={(e) => setUserId(e.target.value)} placeholder="UserId" required />
-        <input type="text" onChange={(e) => setTitle(e.target.value)} placeholder="Title" required />
-        <input type="text" onChange={(e) => setContent(e.target.value)} placeholder="Content(optional)" />
-        <input type="text" onChange={(e) => setStartTime(e.target.value)} placeholder="StartTime(optional)" />
-        <input type="text" onChange={(e) => setEndTime(e.target.value)} placeholder="endTime(optional)" />
-        <input type="text" onChange={(e) => setColor(e.target.value)} placeholder="color(optional)" />
-        <button type="submit">Create Todo</button>
+      <h1 style={{ color: 'orange' }}>Todo Control</h1>
+      <form onSubmit={todoSubmitHandler} style={{ display: 'flex', flexDirection: 'column', width: '335px' }}>
+        <div style={{ display: 'flex' }}>
+          <input type="text" value={userId} onChange={(e) => setUserId(e.target.value)} placeholder="UserId" required />
+          <input type="text" value={todoId} onChange={(e) => setTodoId(e.target.value)} placeholder="todoId(delete only)" />
+        </div>
+        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Title" />
+        <input type="text" value={content} onChange={(e) => setContent(e.target.value)} placeholder="Content(optional)" />
+        <input type="text" value={startTime} onChange={(e) => setStartTime(e.target.value)} placeholder="StartTime(optional)" />
+        <input type="text" value={endTime} onChange={(e) => setEndTime(e.target.value)} placeholder="endTime(optional)" />
+        <input type="text" value={color} onChange={(e) => setColor(e.target.value)} placeholder="color(optional)" />
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <button type="submit" name="create">
+            Create Todo
+          </button>
+          <button type="submit" name="read">
+            Read Todo
+          </button>
+          <button type="submit" name="update">
+            Update Todo
+          </button>
+          <button type="submit" name="delete">
+            Delete Todo
+          </button>
+        </div>
       </form>
     </div>
   );
