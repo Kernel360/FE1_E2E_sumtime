33f2b47ee224a7d892ef309ad9374850265a2762
dbshaejin <dbshaejin@gmail.com>
2024-08-01 11:51:53 +0900

    feat: currentTimeLine 구현(각 slot요소 height 동일하게 구현)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import Timetable from '@/timetable/components/Timetable';
+import { taskList, startTime, endTime, slotTime } from '@/timetable/mocks/timetableMockData';
+
 export default function Home() {
-  return <div>sumday</div>;
+  return (
+    <>
+      <div>sumday</div>
+      <Timetable
+        startTime={startTime}
+        endTime={endTime}
+        slotTime={slotTime}
+        taskList={taskList}
+        height="1400px"
+        displayCurrentTime
+        timetableType="COLUMN"
+      />
+    </>
+  );
 }
