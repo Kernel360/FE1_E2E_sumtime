f5c3723fc5c106994e8eb865d5b3ae94b36d28d2
Yejin <78341274+Yejin0O0@users.noreply.github.com>
2024-08-01 09:51:34 +0900

    feat: index 페이지 ui 완성 (#33)

---
+++ src/app/HomePage.styled.ts
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const HomePage = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const HomeLogo = styled.img`
+  width: 200px;
+  cursor: pointer;
+`;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,28 @@
 'use client';
 
+import Link from 'next/link';
+import Button from '@mui/material/Button';
+import logo from '@/assets/images/sumtimeLogo.png';
+import { Flex } from '@/components/common';
+import * as S from './HomePage.styled';
+
 export default function Home() {
-  return <div>sumday</div>;
+  return (
+    <S.HomePage>
+      <Flex $direction="column" $gap="30px" $justify="center" $align="center">
+        <S.HomeLogo src={logo.src} alt="logo" />
+        <div>
+          <p>당신의 하루를 더하세요</p>
+        </div>
+        <Flex>
+          <Link href="/login">
+            <Button variant="text">로그인하기</Button>
+          </Link>
+          <Link href="/signup">
+            <Button variant="text">회원가입하기</Button>
+          </Link>
+        </Flex>
+      </Flex>
+    </S.HomePage>
+  );
 }
