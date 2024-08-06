fe4c912c65441d15c4242b2097e7902d3d7cd2b9
dbshaejin <dbshaejin@gmail.com>
2024-08-06 18:03:32 +0900

    feat: Timetable mockData기반 연동

---
+++ src/components/TimeTable/TimeTable.styled.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const TimeTableSection = styled.div`
+  padding: 20px;
+  width: 50%;
+  height: 484px;
+  /* width: 100%; // 이건 COLUMN일 때 */
+  /* height: 100%; // 이건 COLUMN일 때 */
+`;
