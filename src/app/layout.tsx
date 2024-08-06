2c6be22440bdf9129649db51d48689743b196b4a
godhyzzang <109357302+godhyzzang@users.noreply.github.com>
2024-08-06 16:34:54 +0900

    feat :  todo api 연결 (tanstack query X) (#62)
    * feat : todo fetch 적용
    
    * refactor : api 에 맞게 변수이름 수정
    
    * feat : todo CRUD 연결
    
    * feat: create row type timetable component & merge (#55)
    
    * feat: undo
    * feat: taskSlotItem 적용
    * feat: width, height 조절
    * refactor: col type 컴포넌트를 row type으로 대체
    * refactor: 파일 분리 및 컴포넌트 재사용
    * refactor: CurrentTimeLine 파일 분리
    * chore: 오타 수정 및 공백 제거
    * chore: 쓰지 않는 주석 제거
    * fix: eslint 에러 제거
    ---------
    Co-authored-by: dbshaejin <dbshaejin@gmail.com>
    
    * feat: next auth 프로젝트 연동 (#49)
    
    * feat: google OAuth 연동
    
    * feat: google OAuth 공식문서 기반 연동
    
    * chore: AuthProvider로 컴포넌트 이름 변경
    
    * chore: 배포 빌드 에러 해결을 위한 package.json 세팅 (#59)
    
    * chore: libsql 버전 다운그레이드
    
    * feat:  tanstack query 적용 (#61)
    
    * feat: layout.tsx에 TanstackQuery 적용
    
    * feat: .env 제거
    
    * test: queryFn eslint error 테스트용
    
    * feat: tanstack query 적용(getUserId)
    
    * feat: user api 호출 분리(calls)
    
    * fix: user api 호출 수정
    
    * refactor: todo call 분리
    
    * feat: 테스팅 페이지에 tanstack query 적용 완료
    
    * refactor: try/catch 적용 및 User관련 query 반복호출 제거
    
    * refactor: todo axios 함수명 일관되도록 변경
    
    * refactor: todo axios 함수명 일관되도록 변경
    ---------
    * feat : todo fetch 적용
    
    * fix: PR 충돌로 없어진 AuthProvider 복구
    
    * fix : axios 통신 에러 해결
    
    * refactor : 주석 수정
    
    * refactor : key에 쓸데없는 코드 삭제
    
    ---------
    
    Co-authored-by: dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
    Co-authored-by: Yejin <78341274+Yejin0O0@users.noreply.github.com>
    Co-authored-by: Jason <69420498+enigsuss@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,25 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import ReactQueryProviders from '@/utils/ReactQueryProviders';
+import AuthProvider from '@/components/common/AuthProvider';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/lib/auth';
+
 
 export const metadata: Metadata = {
   title: 'sumday',
   description: 'kernel FE E2E project',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const session = await getServerSession(authOptions);
+
   return (
     <html lang="kr">
       <body>
-        <ReactQueryProviders>{children}</ReactQueryProviders>
+        <AuthProvider session={session}>
+          <ReactQueryProviders>{children}</ReactQueryProviders>
+        </AuthProvider>
       </body>
     </html>
   );
