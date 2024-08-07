18c4ed4cd996ed37bc808a93d9a40a483b9417ea
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-08-07 10:59:51 +0900

    chore: test코드 단어 수정

--- src/app/timetable/utils/tests/parseHeightString.test.ts
+++ src/app/timetable/utils/tests/parseHeightString.test.ts
@@ -2,17 +2,17 @@ import { describe, expect, test } from '@jest/globals';
 import { parseSizeFormat, parseSizeValue } from '..';
 import { FORMAT_LIST } from '../../constants';
 
-describe('Timetable의 height 값 나누기', () => {
-  describe('height 값에서 format 가져오기', () => {
-    const height = 1000;
+describe('Timetable의 size 값 나누기', () => {
+  describe('size 값에서 format 가져오기', () => {
+    const size = 1000;
 
     FORMAT_LIST.forEach((format) => {
-      test(`height으로 ${height}${format} 이 주어졌을 때 ${format}을 분리할 수 있다.`, () => {
+      test(`size으로 ${size}${format} 이 주어졌을 때 ${format}을 분리할 수 있다.`, () => {
         // given
-        const heightString = `${height}${format}`;
+        const sizeString = `${size}${format}`;
 
         // when
-        const result = parseSizeFormat(heightString);
+        const result = parseSizeFormat(sizeString);
 
         // then
         expect(result).toEqual(format);
@@ -20,17 +20,17 @@ describe('Timetable의 height 값 나누기', () => {
     });
   });
 
-  describe('height 값에서 value 가져오기', () => {
-    const heightValueList = [100, 95, 0.5, 48.33333, 3];
+  describe('size 값에서 value 가져오기', () => {
+    const sizeValueList = [100, 95, 0.5, 48.33333, 3];
 
-    heightValueList.forEach((heightValue) => {
+    sizeValueList.forEach((sizeValue) => {
       FORMAT_LIST.forEach((format) => {
-        const height = `${heightValue}${format}`;
-        test(`height으로 ${height}이 주어졌을 때 값 ${heightValue}을 분리할 수 있다.`, () => {
+        const size = `${sizeValue}${format}`;
+        test(`size으로 ${size}이 주어졌을 때 값 ${sizeValue}을 분리할 수 있다.`, () => {
           // when
-          const result = parseSizeValue(height);
+          const result = parseSizeValue(size);
           // then
-          expect(result).toEqual(heightValue);
+          expect(result).toEqual(sizeValue);
         });
       });
     });
