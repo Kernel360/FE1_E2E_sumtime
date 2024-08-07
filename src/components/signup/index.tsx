1483987d8da0678a10d16bfb947ecdee472b6279
Yejin0O0 <yyj11kr@naver.com>
2024-08-07 18:08:15 +0900

    feat: 회원가입 기능 구현

--- src/components/signup/index.tsx
+++ src/components/signup/index.tsx
@@ -3,10 +3,29 @@
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import logo from '@/assets/images/sumtimeLogo.png';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { createUser } from '@/app/apiTest/calls/userCalls';
 import * as S from './Signup.styled';
 
 function SignupSection() {
   // const [validation, setValidation] = useState(true);
+  const [data, setData] = useState({
+    email: '',
+    password: '',
+    nickname: '',
+  });
+
+  const router = useRouter();
+
+  const registerUserHandler = async (event: React.FormEvent) => {
+    event.preventDefault();
+    const userInfo = await createUser(data.email, data.password, data.nickname);
+    if (userInfo) {
+      alert(`환영합니다, ${data.nickname}님!`);
+      router.push('/login');
+    }
+  };
 
   return (
     <S.SignupSection>
@@ -21,6 +40,9 @@ function SignupSection() {
           // helperText={validation ? '' : '올바른 형식의 이메일을 입력해주세요'}
           variant="standard"
           type="email"
+          onChange={(e) => {
+            setData({ ...data, email: e.target.value });
+          }}
         />
       </S.SignupInputDiv>
       <S.SignupInputDiv>
@@ -32,19 +54,12 @@ function SignupSection() {
           // helperText={validation ? '' : '올바른 형식의 비밀번호를 입력해주세요'}
           variant="standard"
           type="password"
+          onChange={(e) => {
+            setData({ ...data, password: e.target.value });
+          }}
         />
       </S.SignupInputDiv>
-      <S.SignupInputDiv>
-        <TextField
-          // error={!validation}
-          fullWidth
-          id="outlined-error-helper-text"
-          label="비밀번호 확인"
-          // helperText={validation ? '' : '비밀번호가 일치하지 않습니다'}
-          variant="standard"
-          type="password"
-        />
-      </S.SignupInputDiv>
+
       <S.SignupInputDiv>
         <TextField
           // error={!validation}
@@ -54,10 +69,15 @@ function SignupSection() {
           // helperText={validation ? '' : '0자 이내만 입력 가능합니다'}
           variant="standard"
           type="text"
+          onChange={(e) => {
+            setData({ ...data, nickname: e.target.value });
+          }}
         />
       </S.SignupInputDiv>
 
-      <Button variant="outlined">회원가입</Button>
+      <Button variant="outlined" onClick={(e) => registerUserHandler(e)}>
+        회원가입
+      </Button>
     </S.SignupSection>
   );
 }
