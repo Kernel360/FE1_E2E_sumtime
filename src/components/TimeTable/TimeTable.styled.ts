d5210050bb6bccd673e0d9b459f9c70891dcaee2
dbshaejin <dbshaejin@gmail.com>
2024-08-07 11:58:20 +0900

    chore: 불필요 주석 제거

--- src/components/TimeTable/TimeTable.styled.ts
+++ src/components/TimeTable/TimeTable.styled.ts
@@ -3,7 +3,5 @@ import styled from '@emotion/styled';
 export const TimeTableSection = styled.div`
   padding: 20px 20px 0px;
   width: 50%;
-
-  /* width: 100%; // 이건 COLUMN일 때 */
-  height: 100%; // 이건 COLUMN일 때
+  height: 100%;
 `;
