a3ceba1009a228dc97a390531ebbc7e0b9b9f660
Yejin0O0 <yyj11kr@naver.com>
2024-08-08 15:33:43 +0900

    fix: Session user id type 수정

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -4,7 +4,7 @@ import NextAuth from 'next-auth';
 declare module 'next-auth' {
   interface Session {
     user: {
-      id: string | number;
+      id: number;
       name: string;
       email: string;
     };
