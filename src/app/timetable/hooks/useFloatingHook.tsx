49293c6558f9741b927b66cc41e6aa9e1ff6c321
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-08-08 15:46:42 +0900

    chore: 코드 리뷰를 위한 pr

--- src/app/timetable/hooks/useFloatingHook.tsx
+++ src/app/timetable/hooks/useFloatingHook.tsx
@@ -1,8 +1,10 @@
-import { useFloating, offset, useDismiss, useInteractions } from '@floating-ui/react';
-import { useState } from 'react';
+/* eslint-disable consistent-return */
+import { useFloating, offset, useDismiss, useInteractions, useHover } from '@floating-ui/react';
+import { useEffect, useState, useCallback } from 'react';
 
 function useFloatingInReference() {
   const [isFloatingTargetVisible, setIsTooltipVisible] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
   const [clickPosition, setClickPosition] = useState({ x: 0, y: 0 });
 
   const { refs, floatingStyles, context } = useFloating({
@@ -26,17 +28,54 @@ function useFloatingInReference() {
       ),
     ],
     open: isFloatingTargetVisible,
-    onOpenChange: setIsTooltipVisible,
+    onOpenChange(open, event, reason) {
+      console.log('isClickedin onOpenChange', isClicked);
+
+      if (isClicked && reason === 'hover') {
+        return;
+      }
+
+      setIsTooltipVisible(open);
+    },
   });
 
   const dismiss = useDismiss(context, { outsidePress: true });
-  const { getReferenceProps, getFloatingProps } = useInteractions([dismiss]);
+  const hover = useHover(context);
+  const { getReferenceProps, getFloatingProps } = useInteractions([dismiss, hover]);
 
-  const onFloating = (event: React.MouseEvent<HTMLButtonElement>) => {
+  const onFloating = (event: React.MouseEvent) => {
     const { clientX, clientY } = event;
     setClickPosition({ x: clientX, y: clientY });
     setIsTooltipVisible(true);
+    setIsClicked(true);
   };
+
+  const handleMouseMove = (event: MouseEvent) => {
+    const { clientX, clientY } = event;
+
+    if (!isClicked) {
+      console.log('isClicked', isClicked);
+
+      setIsTooltipVisible(true);
+      setClickPosition({ x: clientX + 10, y: clientY + 10 });
+    }
+  };
+
+  useEffect(() => {
+    const refElement = refs.reference?.current as HTMLElement | null;
+
+    if (!refElement) {
+      return;
+    }
+
+    if (refElement) {
+      refElement.addEventListener('mousemove', handleMouseMove)!;
+
+      return () => {
+        refElement.removeEventListener('mousemove', handleMouseMove);
+      };
+    }
+  }, []);
 
   return { refs, floatingStyles, getReferenceProps, getFloatingProps, onFloating, isFloatingTargetVisible };
 }
