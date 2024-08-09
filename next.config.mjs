988faabf8fdcb2daeac71f7704c661de841d84ae
Yejin0O0 <yyj11kr@naver.com>
2024-08-09 16:01:02 +0900

    fix: github action build fail을 해결하기 위한 config 추가

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,9 @@ const nextConfig = {
         hostname: 'lh3.googleusercontent.com',
       },
     ],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 };
 
