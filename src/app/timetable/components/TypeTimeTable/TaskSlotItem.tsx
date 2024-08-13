cd4bb0af3e8bc743a97391d8a19868b383fcd2c0
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-08-13 11:41:00 +0900

    refactor: PopoverContent 분리

--- src/app/timetable/components/TypeTimeTable/TaskSlotItem.tsx
+++ src/app/timetable/components/TypeTimeTable/TaskSlotItem.tsx
@@ -1,13 +1,11 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { useContext, useRef, useEffect, useState } from 'react';
-import { format } from 'date-fns';
 import usePopoverFloating from '../../hooks/usePopoverFloating';
 import { calculateTaskOffsetAndHeightPercent, generateClassNameWithType, getRandomColor, getTaskColor } from '../../utils';
 import { BaseTask } from '../Timetable.type';
 import { TypeContext, PopoverTypeContext, TaskSlotContext, TaskThemeContext } from '../../contexts';
 import styles from './TypeTimeTable.module.scss';
-import closeImage from '../../assets/close.png';
-import scheduleImage from '../../assets/schedule.png';
+import PopoverContent from './PopoverContent';
 
 interface TaskSlotItemProps<T extends BaseTask> {
   taskItem: T;
@@ -18,9 +16,6 @@ interface TaskSlotItemProps<T extends BaseTask> {
   slotTime: number;
 }
 
-const text =
-  '국가유공자·상이군경 및 전몰군경의 유가족은 법률이 정하는 바에 의하여 우선적으로 근로의 기회를 부여받는다. 모든 국민은 신속한 재판을 받을 권리를 가진다. 형사피고인은 상당한 이유가 없는 한 지체없이 공개재판을 받을 권리를 가진다.이 헌법은 1988년 2월 25일부터 시행한다. 다만, 이 헌법을 시행하기 위하여 필요한 법률의 제정·개정과 이 헌법에 의한 대통령 및 국회의원의 선거 기타 이 헌법시행에 관한 준비는 이 헌법시행 전에 할 수 있다.';
-
 function TaskSlotItem<T extends BaseTask>({
   taskItem,
   shouldDisplayTaskContent,
@@ -28,7 +23,7 @@ function TaskSlotItem<T extends BaseTask>({
   slotEndTime,
   slotTime,
 }: TaskSlotItemProps<T>) {
-  const { startTime, endTime, title, content } = taskItem;
+  const { startTime, endTime, title } = taskItem;
   const taskSlotRef = useRef<HTMLDivElement>(null);
   const [isContentVisible, setIsContentVisible] = useState(false);
   const type = useContext(TypeContext);
@@ -107,28 +102,13 @@ function TaskSlotItem<T extends BaseTask>({
         </div>
       </button>
       {isFloatingTargetVisible && (
-        <div
-          {...getFloatingProps()}
-          ref={refs.setFloating}
-          style={{
-            ...floatingStyles,
-          }}
-          className={styles.popoverLayout}
-        >
-          <div className={styles.buttonLayout}>
-            <button type="button" onClick={hidePopover} className={styles.closeButton}>
-              <img src={closeImage.src} alt="close button" />
-            </button>
-          </div>
-          <div className={styles.popoverTitle}>{title}</div>
-          <div className={styles.scheduleLayout}>
-            <img src={scheduleImage.src} alt="close button" className={styles.scheduleIcon} />
-            <p className={styles.scheduleContent}>
-              {format(startTime, "hh':'mm")} - {format(endTime, "hh':'mm")}
-            </p>
-          </div>
-          {content && <div style={{ marginTop: '10px', fontSize: '14px' }}>{text}</div>}
-        </div>
+        <PopoverContent
+          taskItem={taskItem}
+          hidePopover={hidePopover}
+          floatingStyles={floatingStyles}
+          getFloatingProps={getFloatingProps}
+          refs={refs}
+        />
       )}
     </div>
   );
