4ecd166540f2db770eda491dbf47b13ed007625a
Jason <enigsuss@gmail.com>
2024-08-14 17:12:43 +0900

    fix: pagination 날짜를 이동해도 calendar 날짜가 수정되지 않는 문제 수정

--- src/components/todo/index.tsx
+++ src/components/todo/index.tsx
@@ -72,6 +72,7 @@ export default function Todo() {
       >
         <DateCalendar
           defaultValue={new Date()}
+          value={displayingDate}
           onChange={(value) => {
             setDisplayingDate(value);
             toggleIsCalendarOpen();
