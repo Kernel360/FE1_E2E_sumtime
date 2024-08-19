c8824d42f4875314db2205eead53e07b02b39f71
dbshaejin <dbshaejin@gmail.com>
2024-08-19 17:22:53 +0900

    feat: 마이페이지 레이아웃 구현

--- src/app/mypage/layout.tsx
+++ src/app/mypage/layout.tsx
@@ -1,17 +1,50 @@
+'use client';
+
+import Header from '@/components/Header';
+import { Box, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import { ReactNode } from 'react';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import CategoryOutlinedIcon from '@mui/icons-material/CategoryOutlined';
+import LiveHelpOutlinedIcon from '@mui/icons-material/LiveHelpOutlined';
+import ExitToAppOutlinedIcon from '@mui/icons-material/ExitToAppOutlined';
+import { signOut } from 'next-auth/react';
+import * as S from '@/app/mypage/MyPage.styled';
 
 interface MyPageLayoutProps {
   children: ReactNode;
 }
 
+const MENU_ITEM = [
+  { href: '/mypage/account', icon: <AccountCircleOutlinedIcon color="action" />, text: '계정' },
+  { href: '/mypage/category', icon: <CategoryOutlinedIcon />, text: '카테고리' },
+  { href: '/mypage/faq', icon: <LiveHelpOutlinedIcon />, text: 'FAQ' },
+];
+
 function MyPageLayout({ children }: MyPageLayoutProps) {
   return (
     <>
-      <div>mui 사이드바 영역</div>
-      <div>
-        <p>main content 영역</p>
-        {children}
-      </div>
+      <Header />
+      <S.Container>
+        <Box display="flex" flexDirection="column" gap="30px" sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+          {MENU_ITEM.map((item) => (
+            <ListItem key={item.text} disablePadding>
+              <ListItemButton href={item.href}>
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText primary={item.text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+          <ListItem disablePadding>
+            <ListItemButton onClick={() => signOut({ callbackUrl: '/landing' })}>
+              <ListItemIcon>
+                <ExitToAppOutlinedIcon />
+              </ListItemIcon>
+              <ListItemText primary="로그아웃" />
+            </ListItemButton>
+          </ListItem>
+        </Box>
+        <S.ChildrenWrapper>{children}</S.ChildrenWrapper>
+      </S.Container>
     </>
   );
 }
