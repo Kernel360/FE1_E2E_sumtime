4d0a7fb53b942b7df3f310e3dc31715393ad5f2f
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-08-19 11:35:55 +0900

    feat: 라이브러리로 변경

--- src/components/TimeTable/index.tsx
+++ src/components/TimeTable/index.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import Timetable from '@/app/timetable/components';
-import { startTime, endTime } from '@/app/timetable/mocks/timetableMockData';
+import Timetable, { getTodayFromTime } from 'react-custom-timetable';
 import { useContext } from 'react';
 import { TodoDataContext } from '@/context/TodoDataContext';
 import { convertTodosForTimetable } from '@/utils/convertTodosForTimetable';
@@ -10,6 +9,9 @@ import * as S from './TimeTable.styled';
 function TimeTable() {
   const { todoListData } = useContext(TodoDataContext);
   const allTodosForTimetable = convertTodosForTimetable(todoListData);
+  const startTime = getTodayFromTime(0, 0, 1);
+  const endTime = getTodayFromTime(23, 59, 59);
+
   return (
     <S.TimeTableSection>
       {allTodosForTimetable && (
