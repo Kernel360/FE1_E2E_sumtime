53419f136d6bafe9a2db12981d3933c794149ff5
godhyzzang <109357302+godhyzzang@users.noreply.github.com>
2024-08-20 16:29:05 +0900

    feat : react-redux 적용

---
+++ src/lib/store.ts
@@ -0,0 +1,16 @@
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './features/counter/counterSlice';
+
+export const makeStore = () => {
+  return configureStore({
+    reducer: {
+      counter: counterReducer, // 'counter'라는 이름으로 리듀서를 등록
+    },
+  });
+};
+
+// Infer the type of makeStore
+export type AppStore = ReturnType<typeof makeStore>;
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<AppStore['getState']>;
+export type AppDispatch = AppStore['dispatch'];
