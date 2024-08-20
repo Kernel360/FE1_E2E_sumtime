55c8d5c8f7503d7f5abe0ca0705e19fbf522872a
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-08-20 17:43:08 +0900

    feat: ColorPicker 컴포넌트에 color palette 추가

---
+++ src/components/ColorPicker/constants.ts
@@ -0,0 +1,11 @@
+const DEFAULT_COLOR_PALETTE = [
+  '#FF0000', // red
+  '#FFA500', // orange
+  '#FFFF00', // yellow
+  '#008000', // green
+  '#0000FF', // blue
+  '#000080', // navy
+  '#800080', // purple
+];
+
+export { DEFAULT_COLOR_PALETTE };
--- src/components/ColorPicker/index.tsx
+++ src/components/ColorPicker/index.tsx
@@ -1,18 +1,63 @@
-import { forwardRef } from 'react';
+import { forwardRef, useState } from 'react';
 import { HexColorPicker } from 'react-colorful';
-import * as S from './ColorPicker.styled';
+import { getLocalItem, setLocalItem } from '@/utils';
+import * as CommonStyle from '@/components/common';
+import * as ColorPickerStyle from './ColorPicker.styled';
+import { DEFAULT_COLOR_PALETTE } from './constants';
+
+const S = { ...CommonStyle, ...ColorPickerStyle };
 
 interface ColorPickerProps extends React.ComponentPropsWithRef<'div'> {
   color: string;
   setColor: React.Dispatch<React.SetStateAction<string>>;
+  showPalette?: boolean;
 }
 
 function ColorPicker(props: ColorPickerProps, ref: React.Ref<HTMLDivElement>) {
-  const { color, setColor, ...otherProps } = props;
+  const { color, setColor, showPalette = false, ...otherProps } = props;
+  const [localColorPalette, setLocalColorPalette] = useState<string[]>(getLocalItem<string[]>('colorPalette') ?? []);
+  const [colorPalette, setColorPalette] = useState<string[]>([...localColorPalette, ...DEFAULT_COLOR_PALETTE].slice(0, 10));
+
+  const handleSaveColor = () => {
+    if (localColorPalette.includes(color)) {
+      return;
+    }
+
+    if (localColorPalette.length >= 10) {
+      localColorPalette.pop();
+    }
+
+    const updatedLocalColorPalette = [color, ...localColorPalette];
+    setLocalColorPalette([...updatedLocalColorPalette]);
+    setLocalItem('colorPalette', [...updatedLocalColorPalette]);
+    setColorPalette([...updatedLocalColorPalette, ...DEFAULT_COLOR_PALETTE].slice(0, 10));
+  };
 
   return (
     <S.ColorPickerLayout {...otherProps} ref={ref}>
       <HexColorPicker color={color} onChange={setColor} />
+      {showPalette && (
+        <>
+          <S.SaveBarLayout $justify="space-between" $align="center">
+            <S.Text $fontSize="14px">Saved color:</S.Text>
+            <S.SaveButton type="button" onClick={handleSaveColor}>
+              save
+            </S.SaveButton>
+          </S.SaveBarLayout>
+          <S.ColorPaletteLayout>
+            {colorPalette.map((paletteColor) => (
+              <S.PaletteColorButton
+                $backgroundColor={paletteColor}
+                type="button"
+                key={paletteColor}
+                onClick={() => {
+                  setColor(paletteColor);
+                }}
+              />
+            ))}
+          </S.ColorPaletteLayout>
+        </>
+      )}
     </S.ColorPickerLayout>
   );
 }
