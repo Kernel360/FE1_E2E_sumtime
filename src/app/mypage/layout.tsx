4ff6c5b9d8a14670fecf975204d27bd2401e8f5a
dbshaejin <dbshaejin@gmail.com>
2024-08-20 10:29:13 +0900

    feat: 선택 된 요소 backgroundColor 입히기

--- src/app/mypage/layout.tsx
+++ src/app/mypage/layout.tsx
@@ -7,37 +7,48 @@ import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined
 import CategoryOutlinedIcon from '@mui/icons-material/CategoryOutlined';
 import LiveHelpOutlinedIcon from '@mui/icons-material/LiveHelpOutlined';
 import ExitToAppOutlinedIcon from '@mui/icons-material/ExitToAppOutlined';
+
 import { signOut } from 'next-auth/react';
 import * as S from '@/app/mypage/MyPage.styled';
+import { usePathname } from 'next/navigation';
 
 interface MyPageLayoutProps {
   children: ReactNode;
 }
 
 const MENU_ITEM = [
-  { href: '/mypage/account', icon: <AccountCircleOutlinedIcon color="action" />, text: '계정' },
-  { href: '/mypage/category', icon: <CategoryOutlinedIcon />, text: '카테고리' },
-  { href: '/mypage/faq', icon: <LiveHelpOutlinedIcon />, text: 'FAQ' },
+  {
+    href: '/mypage/account',
+    icon: <AccountCircleOutlinedIcon sx={{ color: 'black' }} />,
+    text: '계정',
+  },
+  { href: '/mypage/category', icon: <CategoryOutlinedIcon sx={{ color: 'black' }} />, text: '카테고리' },
+  { href: '/mypage/faq', icon: <LiveHelpOutlinedIcon sx={{ color: 'black' }} />, text: 'FAQ' },
 ];
 
 function MyPageLayout({ children }: MyPageLayoutProps) {
+  const pathname = usePathname();
+
   return (
     <>
       <Header />
       <S.Container>
         <Box display="flex" flexDirection="column" gap="30px" sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-          {MENU_ITEM.map((item) => (
-            <ListItem key={item.text} disablePadding>
-              <ListItemButton href={item.href}>
-                <ListItemIcon>{item.icon}</ListItemIcon>
-                <ListItemText primary={item.text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
+          {MENU_ITEM.map((item) => {
+            const isActive = pathname === item.href;
+            return (
+              <ListItem key={item.text} disablePadding>
+                <ListItemButton href={item.href} sx={{ bgcolor: isActive ? 'action.hover' : 'inherit' }}>
+                  <ListItemIcon>{item.icon}</ListItemIcon>
+                  <ListItemText primary={item.text} />
+                </ListItemButton>
+              </ListItem>
+            );
+          })}
           <ListItem disablePadding>
             <ListItemButton onClick={() => signOut({ callbackUrl: '/landing' })}>
               <ListItemIcon>
-                <ExitToAppOutlinedIcon />
+                <ExitToAppOutlinedIcon sx={{ color: 'black' }} />
               </ListItemIcon>
               <ListItemText primary="로그아웃" />
             </ListItemButton>
