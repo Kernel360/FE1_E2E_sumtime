7fb768a9b6a4c37663630edf1af6b161addc00bc
Jason <enigsuss@gmail.com>
2024-08-20 12:46:23 +0900

    feat: 카테고리 프론트 CRUD 구현완료

--- src/components/todo/CategoryField.tsx
+++ src/components/todo/CategoryField.tsx
@@ -5,6 +5,8 @@ import Box from '@mui/material/Box';
 import { Chip, IconButton } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
+import CheckCircleOutlinedIcon from '@mui/icons-material/CheckCircleOutlined';
+import useBooleanState from '@/hooks/utils/useBooleanState';
 
 type Category = {
   id: number;
@@ -12,36 +14,67 @@ type Category = {
   color: string;
 };
 
-const categories: Category[] = [
-  { id: 1, title: '식사', color: 'lightcoral' },
-  { id: 2, title: '공부', color: 'lightblue' },
-  { id: 3, title: '업무', color: 'lightgreen' },
-  { id: 4, title: '휴식', color: 'lightpink' },
-];
-
 export default function CategoryField() {
+  const [categories, setCategories] = React.useState<Category[]>([
+    { id: 1, title: '식사', color: 'lightcoral' },
+    { id: 2, title: '공부', color: 'lightblue' },
+    { id: 3, title: '업무', color: 'lightgreen' },
+    { id: 4, title: '휴식', color: 'lightpink' },
+  ]);
   const [selectedCategory, setSelectedCategory] = React.useState<Category[]>([]);
-  const [editingCategoryId, setEditingCategoryId] = React.useState<number | null>(null);
+  const [editingCategory, setEditingCategory] = React.useState<Category>({ id: 0, title: '', color: '' });
+  const { value: isEditing, toggle } = useBooleanState(false);
 
-  const handleEdit = (id: number) => {
-    setEditingCategoryId(id);
+  const handleEdit = (id: number, title: string, color: string) => {
+    setEditingCategory({ id, title, color });
+    setCategories(categories.map((category) => (category.id === id ? { id, title, color } : category)));
+    toggle();
   };
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>, id: number) => {
-    setSelectedCategory((prev) =>
-      prev.map((category) => (category.id === id ? { ...category, title: e.target.value } : category)),
-    );
+  const handleDelete = (id: number) => {
+    setCategories(categories.filter((category) => category.id !== id));
+  };
+
+  const handleKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      handleEdit(editingCategory.id, editingCategory.title, editingCategory.color);
+    }
   };
 
   return (
     <Box sx={{ margin: '10px 0' }}>
+      <Box sx={{ display: isEditing ? 'flex' : 'none', position: 'relative', alignItems: 'center' }}>
+        <TextField
+          inputProps={{ maxLength: 15 }}
+          focused
+          label="카테고리 수정"
+          size="medium"
+          value={editingCategory.title}
+          onChange={(event) => setEditingCategory({ ...editingCategory, title: event.target.value })}
+          onKeyUp={handleKeyUp}
+          sx={{ width: '100%', length: '10' }}
+        />
+        <Box position="absolute" right={14}>
+          <IconButton
+            size="medium"
+            color="default"
+            onClick={() => handleEdit(editingCategory.id, editingCategory.title, editingCategory.color)}
+          >
+            <CheckCircleOutlinedIcon />
+          </IconButton>
+        </Box>
+      </Box>
+
       <Autocomplete
-        disableClearable
+        sx={{ display: isEditing ? 'none' : 'block' }}
         multiple
+        freeSolo
+        autoHighlight
+        disableClearable
         limitTags={1}
         id="tags-outlined"
         options={categories}
-        getOptionLabel={(option) => option.title}
+        getOptionLabel={(option) => (typeof option === 'string' ? option : option.title)}
         value={selectedCategory}
         filterSelectedOptions
         renderInput={(params) => <TextField {...params} label="카테고리" placeholder="카테고리 검색" />}
@@ -53,7 +86,7 @@ export default function CategoryField() {
                 key={tagProps.key}
                 label={option.title}
                 className={tagProps.className}
-                onDelete={tagProps.onDelete}
+                onDelete={() => setSelectedCategory([])}
                 disabled={tagProps.disabled}
                 tabIndex={tagProps.tabIndex}
                 sx={{ background: option.color, color: 'white', borderRadius: '4px' }}
@@ -74,7 +107,7 @@ export default function CategoryField() {
               <span
                 {...props}
                 style={{
-                  display: editingCategoryId === option.id ? 'none' : 'block',
+                  display: isEditing ? 'none' : 'block',
                   width: '100%',
                   height: '100%',
                   margin: '0',
@@ -82,31 +115,31 @@ export default function CategoryField() {
               >
                 {option.title}
               </span>
-              <TextField
-                focused
-                size="small"
-                sx={{ display: editingCategoryId === option.id ? 'block' : 'none', height: '40px' }}
-                value={option.title}
-                onChange={(e) => handleInputChange(e, option.id)}
-              />
             </Box>
             <Box position="absolute" right={40}>
-              <IconButton size="small" sx={{ color: '#b3b3b3' }} onClick={() => handleEdit(option.id)}>
+              <IconButton
+                size="small"
+                sx={{ color: '#b3b3b3' }}
+                onClick={() => handleEdit(option.id, option.title, option.color)}
+              >
                 <EditIcon />
               </IconButton>
             </Box>
             <Box position="absolute" right={8}>
-              <IconButton size="small" sx={{ color: '#b3b3b3' }}>
+              <IconButton size="small" sx={{ color: '#b3b3b3' }} onClick={() => handleDelete(option.id)}>
                 <DeleteIcon />
               </IconButton>
             </Box>
           </Box>
         )}
-        onChange={(e, value: Category[]) => {
-          if (value.length > 0) {
-            setSelectedCategory([value[value.length - 1]]); // 마지막으로 선택한 카테고리만 선택
-          } else {
-            setSelectedCategory([]);
+        onChange={(e, value) => {
+          const lastValue = value[value.length - 1]; // 이 때, value는 Category이거나 Stirng 타입
+          if (typeof lastValue === 'string') {
+            const newCategory = { id: categories.length + 1, title: lastValue, color: 'lightgray' };
+            setCategories([...categories, newCategory]);
+            setSelectedCategory([newCategory]);
+          } else if (lastValue && !selectedCategory.includes(lastValue)) {
+            setSelectedCategory([lastValue]); // 마지막으로 선택한 카테고리만 선택
           }
         }}
       />
