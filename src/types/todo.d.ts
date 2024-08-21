e2f7707b6cf9aa21212c3b7e716d2e24e3036496
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-08-21 15:24:03 +0900

    feat: TodoDateType 추가

--- src/types/todo.d.ts
+++ src/types/todo.d.ts
@@ -1,2 +1,17 @@
 // src/types/todo.d.ts
 export type TodoModalMode = 'create' | 'update' | '';
+
+export interface TodoDateType {
+  id: number;
+  categoryId: number;
+  userId: number;
+  date: string;
+  startTime: Date | null;
+  endTime: Date | null;
+  taskColor: string | null;
+  title: string;
+  content: string | null;
+  createdAt: string;
+  updatedAt: string;
+  color: string | null;
+}
