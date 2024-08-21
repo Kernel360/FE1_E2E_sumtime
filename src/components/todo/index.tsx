bb0a57f2f05e04f18c089c4206a8b0c01975ac23
godhyzzang <109357302+godhyzzang@users.noreply.github.com>
2024-08-22 07:19:57 +0900

    refactor : 주석 수정

--- src/components/todo/index.tsx
+++ src/components/todo/index.tsx
@@ -19,15 +19,15 @@ export default function Todo() {
   const dispatch = useAppDispatch();
   const { data: session } = useSession();
   const sessionId = session?.user?.id;
-  // pagination, calendar에서 선택한 날짜를 가져옴
+  // TodoPagination, TodoCalendar에서 선택한 날짜를 가져옴
   const params = useParams();
   const { year, month, day } = params;
 
-  // 선택한 날짜를 메모이제이션하여 렌더링마다 새로운 객체가 생성되지 않도록 함
   const displayingDate = useMemo(() => {
     return year && month && day ? new Date(Number(year), Number(month) - 1, Number(day)) : new Date();
-    // return year && month && day ? new Date(Number(year), Number(month) - 1, Number(day)) : null;
   }, [year, month, day]);
+
+  const { data: todoListData = [], isLoading } = useGetTodosMatchingDate(sessionId, displayingDate);
 
   // sessionId가 변경될 때마다 redux store에 저장
   useEffect(() => {
@@ -41,10 +41,7 @@ export default function Todo() {
     dispatch(setDisplayingDate(displayingDate));
   }, [displayingDate, dispatch]);
 
-  // sessionId가 존재할 때만 데이터를 가져옴
-  const { data: todoListData = [], isLoading } = useGetTodosMatchingDate(sessionId, displayingDate);
-
-  // todoListData가 변경될 때마다 todoListData를 업데이트
+  // todoListData가 변경될 때마다 redux store에 저장
   useEffect(() => {
     dispatch(setLoading(isLoading));
     if (!isLoading && todoListData) {
