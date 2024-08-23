606ef90485eb2bab6759dde1c76fb80799c72938
dbshaejin <dbshaejin@gmail.com>
2024-08-23 17:54:24 +0900

    feat: 문구 수정 및 tooltip 포현 요소 제어

--- src/app/mypage/category/CategoryTableBody.tsx
+++ src/app/mypage/category/CategoryTableBody.tsx
@@ -75,7 +75,12 @@ function CategoryTableBody() {
       {categoryList.map((category) => {
         const isDisable = category.isDefault === 1;
         return (
-          <Tooltip key={category.id} open={!!isDisable} title="Add" arrow>
+          <Tooltip
+            key={category.id}
+            open={!!isDisable}
+            title="기본 카테고리는 수정 불가능 합니다."
+            disableHoverListener={!isDisable}
+          >
             <StyledTableRow isDisable={isDisable}>
               <TableCell component="th" scope="row" sx={{ maxWidth: '200px', overflowX: 'auto' }}>
                 {category.title}
