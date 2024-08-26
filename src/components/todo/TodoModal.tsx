1fc416255362c255bea15b2fe0f8796437df1425
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-08-26 10:50:03 +0900

    fix: 삭제후 get 요청 후 404 에러 해결

--- src/components/todo/TodoModal.tsx
+++ src/components/todo/TodoModal.tsx
@@ -9,10 +9,9 @@ import { useQueryClient } from '@tanstack/react-query';
 import { useCreateTodo, useDeleteTodo, useGetOneTodo, useUpdateTodo } from '@/api/hooks/todoHooks';
 import { red } from '@mui/material/colors';
 import { TimePicker } from '@mui/x-date-pickers';
-import { parseISO } from 'date-fns';
+import { parseISO, isValid } from 'date-fns';
 import randomColor from 'randomcolor';
 import CategoryField from '@/components/todo/CategoryField';
-import { isValid } from 'date-fns';
 import { useAppDispatch, useAppSelector } from '@/lib/hooks';
 import { closeModal, selectTodoUI } from '@/lib/todos/todoUISlice'; // Redux 상태 추가
 import { selectTodoData } from '@/lib/todos/todoDataSlice'; // Redux 상태 추가
@@ -120,7 +119,6 @@ export default function TodoModal() {
     } else {
       deleteTodo(todoId, {
         onSuccess: () => {
-          queryClient.invalidateQueries({ queryKey: ['todo', todoId] });
           queryClient.invalidateQueries({ queryKey: ['todos', sessionId] });
           handleCloseModal();
         },
