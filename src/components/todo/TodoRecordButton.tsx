7cca29aa4fb3bb3ef11a9f9e3a81cf952548fc86
Jason <enigsuss@gmail.com>
2024-08-27 14:52:36 +0900

    fix: 주석 제거

--- src/components/todo/TodoRecordButton.tsx
+++ src/components/todo/TodoRecordButton.tsx
@@ -13,8 +13,6 @@ function TodoRecordButton({ toggleRecord, todoId, isProgress, isListProgressing 
   const { displayingDate } = useAppSelector((state) => state.todoData);
   const isRecordBlocked = (!isProgress && isListProgressing) || displayingDate?.toDateString() !== new Date().toDateString();
 
-  // test
-
   return (
     <Box
       boxSizing="border-box"
