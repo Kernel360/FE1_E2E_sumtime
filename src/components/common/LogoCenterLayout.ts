a5335e40c4f548a439fc3b31c478e5b691e30696
Yejin0O0 <yyj11kr@naver.com>
2024-08-27 12:59:42 +0900

    feat: not-found 페이지 생성

---
+++ src/components/common/LogoCenterLayout.ts
@@ -0,0 +1,32 @@
+import styled from '@emotion/styled';
+
+export const logoCenterPage = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const logoCenterLogo = styled.img`
+  width: 200px;
+  cursor: pointer;
+`;
+
+export const logoCenterSection = styled.div`
+  width: 450px;
+  height: 350px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const NotFoundExplainDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  height: 100px;
+`;
