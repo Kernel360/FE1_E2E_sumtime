c8777a77a4e44454474f36727b8c90261768e238
godhyzzang <109357302+godhyzzang@users.noreply.github.com>
2024-08-29 19:28:25 +0900

    fix : 어떤 상황에서도 todoList height변함없게 만들기

--- src/components/todo/TodoList/index.tsx
+++ src/components/todo/TodoList/index.tsx
@@ -49,7 +49,7 @@ function TodoList() {
 
   if (isLoading) {
     return (
-      <Box position="relative" width="100%" height="50%" maxHeight="35%">
+      <Box position="relative" width="100%" height="50%" maxHeight="35%" minHeight="35%">
         <S.TodoComponentsSection>
           <Skeleton width="100%" height="100%" sx={{ transform: 'scale(1, 1)', transformOrigin: '0 0%' }} />
         </S.TodoComponentsSection>
@@ -59,7 +59,7 @@ function TodoList() {
 
   if (todoListData.length === 0) {
     return (
-      <Box position="relative" width="100%" height="50%" maxHeight="35%">
+      <Box position="relative" width="100%" height="50%" maxHeight="35%" minHeight="35%">
         <S.TodoComponentsSection style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
           <EmptyTodoList />
           <OpenCreateTodoModalButton />
@@ -69,7 +69,7 @@ function TodoList() {
   }
 
   return (
-    <Box position="relative" width="100%" height="50%" maxHeight="35%">
+    <Box position="relative" width="100%" height="50%" maxHeight="35%" minHeight="35%">
       <S.TodoComponentsSection>
         <S.PaddingBottomTodoComponentsSection>
           {todoListData.map((todo) => (
