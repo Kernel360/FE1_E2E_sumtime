cf7cf060e734c9bf2c747e0ed63643b8f10e9106
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-08-29 14:12:23 +0900

    style: SelectItem 레이아웃 추가

--- src/components/todo/TodoModal/TodoModal.styled.ts
+++ src/components/todo/TodoModal/TodoModal.styled.ts
@@ -2,7 +2,14 @@ import styled from '@emotion/styled';
 
 export const SelectItemLayout = styled.div`
   display: flex;
-  gap: 10px;
+  justify-content: space-between;
   align-items: center;
   width: 100%;
+  gap: 10px;
+`;
+
+export const SelectItemDescriptionLayout = styled.div`
+  display: flex;
+  gap: 10px;
+  align-items: center;
 `;
